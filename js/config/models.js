98d8cc3d3b263515299acfd3a5b7250f9bbfa263
Ken Huang <khuang.dev@gmail.com>
2020-03-04 10:22:11 -0800

    install AsyncStorage and methods

---
+++ js/config/models.js
@@ -0,0 +1,23 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const createFave = faveId => {
+  try {
+    return AsyncStorage.setItem(
+      `${faveId}`,
+      JSON.stringify({id: faveId, fave_on: new Date()}),
+    );
+  } catch (e) {
+    return false;
+  }
+};
+export const deleteFave = faveId => {
+  return AsyncStorage.removeItem(
+    `${faveId}`,
+    JSON.stringify({id: faveId, fave_on: new Date()}),
+  );
+};
+export const queryFaves = async () => {
+  const keys = await AsyncStorage.getAllKeys();
+  const allStores = await AsyncStorage.multiGet(key);
+  return allStores.filter(store => store[1].includes('faved_on'));
+};
