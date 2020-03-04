79edb43f6f43ac64710187872d7ac859c2ef3b91
Ken Huang <khuang.dev@gmail.com>
2020-03-04 12:51:33 -0800

    update props being passed

--- js/screens/Session/SessionContainer.js
+++ js/screens/Session/SessionContainer.js
@@ -4,40 +4,16 @@ import {View, Text, SafeAreaView} from 'react-native';
 import {Query} from '@apollo/react-components';
 import {gql} from 'apollo-boost';
 
-// const SCHEDULE = gql`
-//   {
-//     allSessions {
-//       id
-//       location
-//       startTime
-//       title
-//       speaker {
-//         id
-//       }
-//     }
-//   }
-// `;
-// export default class SessionContainer extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-// return (
-// <Query query={SCHEDULE}>
-//   {({loading, error, data}) => {
-//     if (loading) return <Text>Loading...</Text>;
-//     if (error) return <Text>Error :(</Text>;
-//     if (data)
-//       return (
-//         <Session
-//           navigation={this.props.navigation}
-//           sessions={data.allSessions}
-//         />
-//       );
-//   }}
-// </Query>
-// );
-//   }
-// }
-
-export default Session;
+export default class SessionContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Session
+        session={this.props.route.params.session}
+        navigation={this.props.navigation}
+      />
+    );
+  }
+}
