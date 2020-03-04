2447b0606a8600fc2cd3cd6f98ec7a394998316c
Ken Huang <khuang.dev@gmail.com>
2020-03-03 18:20:48 -0800

    update about screen styling

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -12,20 +12,25 @@ import styles from './styles';
 const About = () => {
   return (
     <SafeAreaView>
-      <Image
-        style={styles.logo}
-        resizeMode={'contain'}
-        source={require('../../assets/images/r10_logo.png')}
-      />
-      <Text>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          resizeMode={'contain'}
+          source={require('../../assets/images/r10_logo.png')}
+        />
+      </View>
+      <Text style={styles.description}>
         R10 is a conference that focuses on just about any topic related to dev.
       </Text>
       <Text style={styles.header}>Date & Venue</Text>
-      <Text>
+      <Text style={styles.description}>
         The R10 conference will take place on Saturday, December 8, 2018 in
         Vancouver, BC.
       </Text>
       <Text style={styles.header}>Code of Conduct</Text>
+      <View style={styles.copyrightContainer}>
+        <Text>© RED Academy 2020</Text>
+      </View>
     </SafeAreaView>
   );
 };
--- js/screens/About/styles.js
+++ js/screens/About/styles.js
@@ -6,15 +6,30 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 20,
+    margin: 10,
+  },
+  description: {
+    margin: 10,
+  },
+  copyrightContainer: {
+    margin: 10,
+    paddingTop: 20,
+    width: '100%',
+    borderTopWidth: 1,
+    borderColor: '#e6e6e6',
   },
   logo: {
     width: '95%',
     height: 45,
     alignSelf: 'center',
+  },
+  logoContainer: {
+    alignSelf: 'center',
+    width: '95%',
+    paddingTop: 20,
+    paddingBottom: 20,
     borderBottomWidth: 1,
     borderColor: '#e6e6e6',
-    marginTop: 10,
-    paddingBottom: 20,
   },
 });
 
