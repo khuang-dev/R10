de92e68337e2b1d8e818b070001073af45fa05d5
Ken Huang <khuang.dev@gmail.com>
2020-03-05 13:21:04 -0800

    add loader - activityindicator

---
+++ js/components/Loader/Loader.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+
+export default class Loader extends Component {
+  render() {
+    return (
+      <ActivityIndicator style={styles.loader} size="large" color="#0000ff" />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+  },
+});
