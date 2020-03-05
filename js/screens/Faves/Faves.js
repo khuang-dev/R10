b43c81af1859aa5fd36d97ee82504dac2317ce70
Ken Huang <khuang.dev@gmail.com>
2020-03-05 13:40:15 -0800

    add filter to FavesContainer, passed data to Faves to load Schedule

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -6,15 +6,55 @@ import {
   StyleSheet,
   Image,
   SectionList,
+  TouchableOpacity,
 } from 'react-native';
 import styles from './styles';
+import moment from 'moment';
+import {formatSessionData} from './helper/FormatSessionData';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Faves = () => {
+const Faves = ({favedSessions, navigation}) => {
+  const newSessions = formatSessionData(favedSessions);
   return (
     <View>
-      <Text style={styles.emptyStateText}>
-        You haven't faved any sessions yet.
-      </Text>
+      {favedSessions.length === 0 ? (
+        <View>
+          <Text style={styles.emptyStateText}>
+            You haven't faved any sessions yet.
+          </Text>
+        </View>
+      ) : (
+        <View>
+          <SectionList
+            sections={newSessions}
+            keyExtractor={newSessions => newSessions.id}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.sessionContainer}>
+                  <TouchableOpacity
+                    onPress={() =>
+                      navigation.navigate('Session', {
+                        session: item,
+                      })
+                    }>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <View style={styles.locationContainer}>
+                      <Text style={styles.location}>{item.location}</Text>
+                      <MaterialCommunityIcons
+                        style={styles.icon}
+                        name="heart"
+                      />
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              );
+            }}
+            renderSectionHeader={({section: {title}}) => (
+              <Text style={styles.startTime}>{moment(title).format('LT')}</Text>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
--- js/screens/Faves/FavesContainer.js
+++ js/screens/Faves/FavesContainer.js
@@ -1,12 +1,58 @@
 import Faves from './Faves';
 import React, {Component} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import {Query} from '@apollo/react-components';
+import {gql} from 'apollo-boost';
+import {FavesContext} from '../../context';
+import Loader from '../../components/Loader';
 
+const SCHEDULE = gql`
+  {
+    allSessions {
+      id
+      location
+      startTime
+      title
+      description
+      speaker {
+        id
+        bio
+        image
+        name
+        url
+      }
+    }
+  }
+`;
 export default class FavesContainer extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <Faves />;
+    return (
+      <Query query={SCHEDULE}>
+        {({loading, error, data}) => {
+          if (loading) return <Loader />;
+          if (error) return <Text>Error :(</Text>;
+          if (data)
+            return (
+              <FavesContext.Consumer>
+                {({faveIds}) => {
+                  const favedSession = data.allSessions.filter(session =>
+                    faveIds.includes(session.id),
+                  );
+                  return (
+                    <Faves
+                      faveIds={faveIds}
+                      favedSessions={favedSession}
+                      navigation={this.props.navigation}
+                    />
+                  );
+                }}
+              </FavesContext.Consumer>
+            );
+        }}
+      </Query>
+    );
   }
 }
