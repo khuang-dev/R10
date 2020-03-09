b0e6b053810bc8f1dfc0d67a102162c9917907ed
Ken Huang <khuang.dev@gmail.com>
2020-03-08 21:00:33 -0700

    update react-native-apps error

---
+++ js/screens/Map/Map.android.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Text from '../../lib/CustomText';
+
+const Map = () => {
+  return (
+    <Text style={{alignSelf: 'center'}}>
+      Maps is currently not supported on Android
+    </Text>
+  );
+};
+
+export default Map;
